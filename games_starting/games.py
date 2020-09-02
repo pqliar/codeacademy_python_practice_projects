e5f6e3885de48e4e7f88f218736d5aaf8cb330d7
Pecu <pecu.danig@gmail.com>
2020-09-02 19:07:28 -0300

    Finished and tested the 4 games of chance

--- games_starting/games.py
+++ games_starting/games.py
@@ -9,16 +9,11 @@ def coin_flip(guess, bet):
   1st parameter guess: 1 (heads) 2(tails)
   2nd parameter bet: amount of money you are betting
   '''
-  money = 100
   flip = random.randint(1, 2)
-  print(flip)
   if guess == flip:
-      money += bet * 2
-      print("Well done, you have won $" + str(bet * 2) + " congratulations!")
+      return bet * 2
   elif guess != flip:
-     money = money - bet
-     print("Unlucky, you have guessed wrong, you have unfortunately lost $" + str(bet) + ", better luck next time!")
-  return money
+     return -bet
 
 def cho_han(prediction, bet):
   money = 100
@@ -32,12 +27,10 @@ def cho_han(prediction, bet):
   else:
     result_odd = "odd"
   if str.lower(prediction) == result_even or str.lower(prediction) == result_odd:
-    money += bet * 2
-    print("Well done, you have won $" + str(bet * 2) + " congratulations!")
+    return bet * 2
   else:
-    money = money - bet
-    print("Unlucky, you have guessed wrong, you have unfortunately lost $" + str(bet) + ", better luck next time!")
-  return money
+    return -bet
+ 
 
 def highest_card(bet):
   deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12]
@@ -53,18 +46,41 @@ def highest_card(bet):
   draw_cards.append(player_two_pick)
 
   if player_one_pick > player_two_pick:
-    player_one_money += bet * 2
+    return bet * 2
   elif player_two_pick > player_one_pick:
-    player_one_money = player_one_money - bet 
+    return -bet 
   else:
     return 0
 
-  return player_one_money
+
+def roulette(guess, bet):
+  numbers = [00, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+  player_one_spin = random.choice(numbers)
+  # if player guess for a number
+  if player_one_spin == 0 or player_one_spin == 00:
+    return -bet
+  elif player_one_spin == guess and not player_one_spin == 0 or player_one_spin == 00:
+    return bet * 35 
+
+  #if player guess for odd and even
+  try:
+    if str.lower(guess) == "even" and player_one_spin % 2 == 0 and player_one_spin != 0 and player_one_spin != 00:
+      return bet * 2
+    elif str.lower(guess) == "odd" and player_one_spin % 2 == 1 and player_one_spin != 0 and player_one_spin != 00:
+      return bet * 2
+    else:
+      return -bet
+  except TypeError:
+    pass
 
 
 
 
 #Call your game of chance functions here
 
-coin_flip(1, 50)
-cho_han("odd", 25)
+money += coin_flip(1, 50)
+money += cho_han("odd", 25)
+money += highest_card(50)
+money += roulette("even", 50)
+
+print(money)
